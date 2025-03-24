28dc090fc52a186e679e8e7008721c14f06f250f
marsbars086 <marress@nuevaschool.org>
2025-03-24 09:34:31 -0700

    Update RowPatternCounter.svelte

--- src/routes/RowPatternCounter.svelte
+++ src/routes/RowPatternCounter.svelte
@@ -1,9 +1,17 @@
 <script>
-
+    let PatternRows = $state('')
+    let ShowPatternTracker = $state(false)
 </script>
 
 <div class="PatternCounter">
-    <p>Hi everypony</p>
+    <p>Pattern repeats every X rows:</p>
+    <input bind:value={PatternRows} placeholder="#stiches/rows">
+    <button onclick={()=> ShowPatternTracker = true}>submit</button>
+    {#if ShowPatternTracker}
+        <p>Pattern trackerrr</p>
+    {:else}
+        <div></div>
+    {/if}
 </div>
 
 <style>
