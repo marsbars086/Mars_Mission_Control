d1666433b09c03efc4020cdb608c2a33fa6049f5
marsbars086 <marress@nuevaschool.org>
2025-03-25 13:33:14 -0700

    Implement Pattern Counter logic

--- src/routes/RowPatternCounter.svelte
+++ src/routes/RowPatternCounter.svelte
@@ -1,21 +1,80 @@
 <script>
-    let PatternRows = $state('')
+    import Button from "./button.svelte"
+    let DesiredNumPatternRows = $state()
     let ShowPatternTracker = $state(false)
+    let NumPatternIterations = $state(0)
+    let count = $state(0)
+    function changecount(addorsubtract){
+        if (addorsubtract === "add"){
+            count += 1
+        } else {
+            count -= 1
+        }
+        CheckForPatternCompletion($state.snapshot(count))
+    }
+
+    function CheckForPatternCompletion(currentcount){
+        if (currentcount === DesiredNumPatternRows){
+            count = 0
+            NumPatternIterations += 1
+        }
+    }
+
+    function ResetCounter(){
+        ShowPatternTracker = false
+        count = 0
+        NumPatternIterations = 0
+        DesiredNumPatternRows = 0 
+    }
+
 </script>
 
 <div class="PatternCounter">
-    <p>Pattern repeats every X rows:</p>
-    <input bind:value={PatternRows} placeholder="#stiches/rows">
-    <button onclick={()=> ShowPatternTracker = true}>submit</button>
+    <button 
+    onclick={()=> ResetCounter()}
+    > 
+    Reset 
+    </button>
+    <h1>Pattern Counter</h1>
+
+
+
+
+    
     {#if ShowPatternTracker}
-        <p>Pattern trackerrr</p>
+        <p>Row {count} of {DesiredNumPatternRows} completed | Pattern completed {NumPatternIterations} times</p>
+        <Button class = "danger lg" on:click={() => changecount("subtract")}>
+            -
+        </Button>
+        <Button class="primary lg" on:click={() => changecount("add")}>
+            +
+        </Button>
     {:else}
-        <div></div>
+        <div>
+            <div class="PatternInput">
+                <p>Pattern repeats every </p>
+                <input type="number"bind:value={DesiredNumPatternRows} placeholder="#" style="width: 40px; margin:0px">
+                <p>rows</p>
+                <button onclick={()=> ShowPatternTracker = true}>submit</button>
+            </div>
+        </div>
     {/if}
 </div>
 
 <style>
+        h1{
+        font-family: 'Londrina Outline', system-ui;
+        font-size: 50px;
+        font-weight: lighter;
+    }
     .PatternCounter {
         justify-items: center;
+    }
+    .PatternInput {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 300px;
+        justify-content: space-around;
     }
 </style>
